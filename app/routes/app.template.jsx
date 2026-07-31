f0159e22e3a6051ab76b4adca4a39cd96214a8c2
auspicioussoft <info@auspicioussoft.com>
2026-07-31 19:03:20 +0530

    template table

--- app/routes/app.template.jsx
+++ app/routes/app.template.jsx
@@ -17,105 +17,105 @@ import ProductPickerModal from "../components/common/ProductPickerModal";
 import { templatePreview } from "../data/templatePreview";
 
 
-const templates = [
-  {
-    id: 1,
-    type: "quantity_break",
-    title: "Quantity Break",
-    description: "Quantity breaks for the same product",
+// const templates = [
+//   {
+//     id: 1,
+//     type: "quantity_break",
+//     title: "Quantity Break",
+//     description: "Quantity breaks for the same product",
 
-    options: [
-      {
-        id: 1,
-        title: "Single",
-        subtitle: "Standard price",
-        price: 20,
-        variantName: "",
-      },
-      {
-        id: 2,
-        title: "Duo",
-        subtitle: "You save 15%",
-        badge: "SAVE £6",
-        price: 34,
-        comparePrice: 40,
-        popular: true,
-        variantName: "Color",
-        variants: [
-          { label: "Black", value: "black"},
-          { label: "Blue", value: "blue"},
-          { label: "Red", value: "red" },
-        ],
-      },
-    ],
-  },
+//     options: [
+//       {
+//         id: 1,
+//         title: "Single",
+//         subtitle: "Standard price",
+//         price: 20,
+//         variantName: "",
+//       },
+//       {
+//         id: 2,
+//         title: "Duo",
+//         subtitle: "You save 15%",
+//         badge: "SAVE £6",
+//         price: 34,
+//         comparePrice: 40,
+//         popular: true,
+//         variantName: "Color",
+//         variants: [
+//           { label: "Black", value: "black"},
+//           { label: "Blue", value: "blue"},
+//           { label: "Red", value: "red" },
+//         ],
+//       },
+//     ],
+//   },
 
-  {
-    id: 2,
-    type: "bxgy",
-    title: "Buy X Get Y",
-    description: "Buy X, Get Y offer",
+//   {
+//     id: 2,
+//     type: "bxgy",
+//     title: "Buy X Get Y",
+//     description: "Buy X, Get Y offer",
 
-    freeGift: true,
+//     freeGift: true,
 
-    options: [
-      {
-        id: 1,
-        title: "Buy 1 Get 1",
-        badge: "SAVE 50%",
-        price: 20,
-        comparePrice: 40,
-      },
-      {
-        id: 2,
-        title: "Buy 2 Get 3",
-        badge: "SAVE 60%",
-        price: 40,
-        comparePrice: 100,
-        freeGift: true
-      },
-    ],
-  },
+//     options: [
+//       {
+//         id: 1,
+//         title: "Buy 1 Get 1",
+//         badge: "SAVE 50%",
+//         price: 20,
+//         comparePrice: 40,
+//       },
+//       {
+//         id: 2,
+//         title: "Buy 2 Get 3",
+//         badge: "SAVE 60%",
+//         price: 40,
+//         comparePrice: 100,
+//         freeGift: true
+//       },
+//     ],
+//   },
 
-  {
-    id: 3,
-    type: "bundle",
-    title: "Product Bundle",
-    description: "Bundle multiple products",
+//   {
+//     id: 3,
+//     type: "bundle",
+//     title: "Product Bundle",
+//     description: "Bundle multiple products",
 
-    options: [
-      {
-        id: 1,
-        title: "1 Pack",
-        price: 20,
-        product: {
-          title: "Basic T-Shirt",
-          image: "https://picsum.photos/80",
-          variants: [
-            { label: "Black", value: "black" },
-            { label: "Blue", value: "blue" },
-          ],
-        }
-      },
-      {
-        id: 2,
-        title: "2 Pack",
-        badge: "SAVE £6",
-        price: 34,
-        comparePrice: 40,
+//     options: [
+//       {
+//         id: 1,
+//         title: "1 Pack",
+//         price: 20,
+//         product: {
+//           title: "Basic T-Shirt",
+//           image: "https://picsum.photos/80",
+//           variants: [
+//             { label: "Black", value: "black" },
+//             { label: "Blue", value: "blue" },
+//           ],
+//         }
+//       },
+//       {
+//         id: 2,
+//         title: "2 Pack",
+//         badge: "SAVE £6",
+//         price: 34,
+//         comparePrice: 40,
 
-        product: {
-          title: "Basic T-Shirt",
-          image: "https://picsum.photos/80",
-          variants: [
-            { label: "Black", value: "black" },
-            { label: "Blue", value: "blue" },
-          ],
-        },
-      },
-    ],
-  },
-];
+//         product: {
+//           title: "Basic T-Shirt",
+//           image: "https://picsum.photos/80",
+//           variants: [
+//             { label: "Black", value: "black" },
+//             { label: "Blue", value: "blue" },
+//           ],
+//         },
+//       },
+//     ],
+//   },
+// ];
 
 const availableProducts = [
   {
@@ -143,23 +143,26 @@ const availableProducts = [
 export default function Templates() {
   const navigate = useNavigate();
 
-  // const [templates, setTemplates] = useState([]);
+  const [templates, setTemplates] = useState([]); 
 
-  // useEffect(() => {
-  //   fetch("/api/bundle-type")
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       const merged = res.data.map((item) => ({
-  //         id: item._id,
-  //         type: item.typeSlug,
-  //         title: item.typeName,
-  //         description: item.typeDescription,
-  //         ...templatePreview[item.typeSlug],
-  //       }));
+  useEffect(() => {
+     console.log("Fetching bundle types...");
+    fetch("/api/bundle-types")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("Data:", res);
+        const merged = res.data.map((item, index) => ({
+          dbId: item._id,           // MongoDB id
+          id: index + 1,
+          type: item.typeSlug,
+          title: item.typeName,
+          description: item.typeDescription,
+          ...templatePreview[item.typeSlug],
+        }));
 
-  //       setTemplates(merged);
-  //     });
-  // }, []);
+        setTemplates(merged);
+      });
+  }, []);
 
   const [colorSchemes, setColorSchemes] = useState([]);
   const [selectedColorScheme, setSelectedColorScheme] = useState(null);
@@ -337,7 +340,7 @@ export default function Templates() {
             onSelect={handleSelect}
             bundleConfig={bundleConfig}
             updateBundleConfig={updateBundleConfig}
-            openProductPicker={openProductPicker}         
+            openProductPicker={openProductPicker}                   
           />
 
           <ProductPickerModal
